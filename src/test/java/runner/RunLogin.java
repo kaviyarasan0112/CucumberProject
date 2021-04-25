7ede4d389608458dd847b4158a0e39e2b8caad81
Kavi <kavithiyaga@gmail.com>
2021-04-25 14:46:37 +0530

    commented Dataprovider annotation

--- src/test/java/runner/RunLogin.java
+++ src/test/java/runner/RunLogin.java
@@ -14,19 +14,17 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 				 //tags= {"@Test"},
 				 plugin= {"pretty","html:target/cucumber-reports",
 						"json:target/cucumber-reports/Cucumber.json",
-					"junit:target/cucumber-reports/Cucumber.xml"}
+					"junit:target/cucumber-reports/Cucumber.xml"})
 				//plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
-					)
+					
 				 //snippets=SnippetType.CAMELCASE)//To get the step defintion in lower camel case
 
 public class RunLogin extends AbstractTestNGCucumberTests {
 
-	
-	  @DataProvider(parallel=true) 
-	  public Object[][] scenarios(){ 
-	  return super.scenarios();
-	  }
-	 
+					/*
+					 * @DataProvider(parallel=true) public Object[][] scenarios(){ return
+					 * super.scenarios(); }
+					 */
 	
 	/*
 	 * @AfterClass public static void writeExtentReport() { Reporter.; }
